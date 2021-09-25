6ba7f410193918539c353d1d84f61678a6f871cd
myb12 <mohammadyasinbappy@outlook.com>
2021-09-25 14:11:28 +0600

    Burger men item is implemented

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
+import { useState } from 'react/cjs/react.development';
 import Logo from '../../images/logo.svg'
 import './Header.css';
 
 
 
 const Header = () => {
+    const [burgerItem, setBurgerItem] = useState(false);
+
+    const handleClick = () => {
+        setBurgerItem(!burgerItem)
+    }
+
     return (
         <div className="header">
             <div className="logo" >
                 <img src={Logo} alt="" />
             </div>
             <nav>
-                <a href="/#" className="nav-icon">
+                <a href="/#" className="nav-icon" onClick={handleClick}>
                     <FaBars />
                 </a>
-                <a href="/shop" className="nav-item">Shop</a>
-                <a href="/order-review" className="nav-item">Order Review</a>
-                <a href="/manage-inventory" className="nav-item">Manage Inventory</a>
+                <a href="/#" className="nav-item">Shop</a>
+                <a href="/#" className="nav-item">Order Review</a>
+                <a href="/#" className="nav-item">Manage Inventory</a>
             </nav>
+            <div className={burgerItem ? 'burger-menu-item burger-menu-visible' : 'burger-menu-item'}>
+                <a href="/#" className="">Shop</a>
+                <a href="/#" className="">Order Review</a>
+                <a href="/#" className="">Manage Inventory</a>
+            </div>
         </div>
     );
 };
