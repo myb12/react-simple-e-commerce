b86065a7bd19a81279e3b8e003a0f71cc2f320da
myb12 <mohammadyasinbappy@outlook.com>
2021-09-25 22:03:40 +0600

    Logo is added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,16 +14,19 @@ const Header = () => {
 
     return (
         <div className="header">
-            <div className="logo" >
-                <img src={Logo} alt="" />
-            </div>
+
             <nav>
-                <a href="/#" className="nav-icon" onClick={handleClick}>
-                    <FaBars />
-                </a>
-                <a href="/#" className="nav-item">Shop</a>
-                <a href="/#" className="nav-item">Order Review</a>
-                <a href="/#" className="nav-item">Manage Inventory</a>
+                <div>
+                    <a href="/#" className="nav-icon" onClick={handleClick}>
+                        <FaBars />
+                    </a>
+                    <a href="/#" className="nav-item">Shop</a>
+                    <a href="/#" className="nav-item">Order Review</a>
+                    <a href="/#" className="nav-item">Manage Inventory</a>
+                </div>
+                <div className="logo" >
+                    <img src={Logo} alt="" />
+                </div>
             </nav>
             <div className={burgerItem ? 'burger-menu-item burger-menu-visible' : 'burger-menu-item'}>
                 <a href="/#" >Shop</a>
